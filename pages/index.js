57e7bee697365274cb6d324b99ce129c3b7da694
Yohan Rodrigues dos Santos <85412862+yohanrodrigues@users.noreply.github.com>
2025-12-21 18:39:10 +0000

    Mensagem Final

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1> HOME TESTE 0123654</h1>
+    return <h1> Essa mensagem é para dizer que eu estou investindo meu tempo hoje, para que possamos aproveitar mais ainda nosso tempo no futoro. 
+        Hoje o que podemos curtir sem pensar muito financeiramente é reflexo desse mesmo processo do passado. Faço por nós, espero que entenda. Te amo.
+    </h1>
 }
 
 export default Home;
