1ec10d7f1a77eb99f3f9516e3db07a2c00643de5
Yohan Rodrigues dos Santos <85412862+yohanrodrigues@users.noreply.github.com>
2025-12-26 19:23:36 +0000

    pedido para minha rainha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-    return <h1> Essa mensagem é para dizer que eu estou investindo meu tempo hoje, para que possamos aproveitar mais ainda nosso tempo no futoro. Hoje o que podemos curtir sem pensar muito financeiramente é reflexo desse mesmo processo do passado. Faço por nós, espero que entenda. Te amo sua linda!.
-    </h1>
+    return <h1> Busca uma Pizza de Mussarela no Sessé?</h1>
 }
 
 export default Home;
